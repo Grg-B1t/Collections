e46f273e1f1aa8d9509ff948ffb1f8472f02446c
Budhi <gurung.b1t@gmail.com>
2022-06-09 14:17:23 -0400

    hashset is created

---
+++ set.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class set {
+    public static void main(String[] args) {
+        HashSet hs = new HashSet<>();
+
+        hs.add(1);
+        hs.add(5);
+        hs.add(9);
+        hs.add(9);
+        hs.add(10);
+        hs.add("Sam");
+        hs.add("Sam");
+        hs.add("sam");
+
+        System.out.println(hs);
+        System.out.println(hs.size());
+    }
+}
